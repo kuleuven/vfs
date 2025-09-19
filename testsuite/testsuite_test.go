d36b354aa7b6498d76961383474827b7b4f21d7c
Peter Verraedt <peter@verraedt.be>
2025-09-19 15:22:15 +0200

    Increase coverage
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- testsuite/testsuite_test.go
+++ testsuite/testsuite_test.go
@@ -4,8 +4,17 @@ import (
 	"testing"
 
 	"github.com/kuleuven/vfs/fs/nativefs"
+	"github.com/kuleuven/vfs/fs/rootfs"
 )
 
 func TestNativeFS(t *testing.T) {
 	RunTestSuiteAdvanced(t, nativefs.New(t.Context(), t.TempDir()))
+}
+
+func TestRootFS(t *testing.T) {
+	root := rootfs.New(t.Context())
+
+	root.MustMount("/", nativefs.New(t.Context(), t.TempDir()), 0)
+
+	RunTestSuiteAdvanced(t, root)
 }
