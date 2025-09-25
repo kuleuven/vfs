956e33d66944bd2d838c6ec2eba5348315138074
Peter Verraedt <peter@verraedt.be>
2025-09-25 17:04:27 +0200

    Change EBADFD to EINVAL
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- errors.go
+++ errors.go
@@ -9,4 +9,4 @@ var ErrNotSupported = syscall.EOPNOTSUPP
 
 var ErrNotImplemented = fmt.Errorf("%w: not implemented", syscall.EPERM)
 
-var ErrInvalidHandle = fmt.Errorf("%w: invalid handle", syscall.EBADFD)
+var ErrInvalidHandle = fmt.Errorf("%w: invalid handle", syscall.EINVAL)
