9f71a4d238510027e22ae555ff777a2a7179b2fc
Peter Verraedt <peter@verraedt.be>
2025-10-01 14:42:28 +0200

    Add tests for irodsfs package using cobalt
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

---
+++ fs/irodsfs/irods_test.go
@@ -0,0 +1,88 @@
+package irodsfs_test
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"net"
+	"testing"
+
+	"gitea.icts.kuleuven.be/coz/cobalt"
+	"github.com/kuleuven/iron"
+	"github.com/kuleuven/vfs"
+	"github.com/kuleuven/vfs/fs/irodsfs"
+	_ "github.com/kuleuven/vfs/fs/irodsfs"
+	"github.com/kuleuven/vfs/fs/nativefs"
+	_ "github.com/kuleuven/vfs/fs/nativefs"
+	"github.com/sirupsen/logrus"
+)
+
+func runServer(ctx context.Context, dir string) int {
+	cert, err := tls.LoadX509KeyPair("test/cert.pem", "test/key.pem")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	fs := nativefs.New(ctx, dir).(vfs.OpenFileFS) //nolint:forcetypeassert
+
+	config := cobalt.Config{
+		Certificate: cert,
+		Users: []cobalt.User{
+			{
+				Username: "admin",
+				Password: "test",
+				FS:       fs,
+			},
+		},
+	}
+
+	server, err := cobalt.New(config)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	port := l.Addr().(*net.TCPAddr).Port //nolint:forcetypeassert
+
+	go func() {
+		if err := server.Serve(ctx, l); err != nil {
+			if errors.Is(err, context.Canceled) {
+				return
+			}
+
+			logrus.Fatal(err)
+		}
+	}()
+
+	return port
+}
+
+func TestIrodsFS(t *testing.T) {
+	port := runServer(t.Context(), t.TempDir())
+
+	logrus.SetLevel(logrus.DebugLevel)
+
+	client, err := iron.New(t.Context(), iron.Env{
+		Host:            "localhost",
+		Port:            port,
+		Username:        "admin",
+		Password:        "test",
+		Zone:            "cobalt",
+		SSLVerifyServer: "none",
+	}, iron.Option{
+		ClientName:         "test-irods-fs",
+		MaxConns:           2,
+		AllowConcurrentUse: true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testfs := irodsfs.New(t.Context(), "cobalt", client)
+
+	vfs.RunTestSuiteRW(t, testfs)
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.24.0
 
 require (
 	github.com/joshlf/go-acl v0.0.0-20200411065538-eae00ae38531
-	github.com/kuleuven/iron v0.4.5
+	github.com/kuleuven/iron v0.4.7
 	github.com/pkg/sftp v1.13.9
 	github.com/pkg/xattr v0.4.12
 	github.com/sirupsen/logrus v1.9.3
@@ -14,17 +14,28 @@ require (
 )
 
 require (
+	gitea.icts.kuleuven.be/coz/cobalt v0.0.0-20251001123629-c7f236aa0ebb // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/evanphx/json-patch v0.5.2 // indirect
-	github.com/go-openapi/jsonpointer v0.22.0 // indirect
-	github.com/go-openapi/swag/jsonname v0.24.0 // indirect
+	github.com/go-openapi/jsonpointer v0.22.1 // indirect
+	github.com/go-openapi/swag/jsonname v0.25.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/joshlf/testutil v0.0.0-20170608050642-b5d8aa79d93d // indirect
 	github.com/kr/fs v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
-	golang.org/x/term v0.33.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	golang.org/x/exp v0.0.0-20250531010427-b6e5de432a8b // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/term v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	modernc.org/libc v1.65.10 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
+	modernc.org/sqlite v1.37.1 // indirect
 )
